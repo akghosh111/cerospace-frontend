52b6ea6ed89a8cdbeeecfbce966429c34d9cec13
akghosh111 <ghoshkiran2017@gmail.com>
2025-02-13 20:26:06 +0530

    updated footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
       <div className="container mx-auto px-4 flex flex-col md:flex-row justify-between items-center">
         <div className="text-center md:text-left mb-4 md:mb-0">
           <h2 className="text-blue-900 font-bold text-lg">Cerospace</h2>
-          <p className="text-gray-700">Built for SIT Ideathon by Anukiran, Srishty & Gokul</p>
+          <p className="text-gray-700">Built for Codeflow by Anukiran & Koyelika</p>
         </div>
 
         <nav className="flex space-x-4">
